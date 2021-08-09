c5920fb7db162d5d1da9b6cc1a8843f62eb1d80b
fishyydev <19339@wc.school.nz>
2021-08-09 17:47:03 +1200

    changed default text and favicon
    lmao

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>There is nothing here.</h1>
       <h2>
         Deployed with{' '}
         <a
@@ -30,24 +30,10 @@ function App() {
         <a
           href="https://github.com/vercel/vercel/tree/main/examples/create-react-app"
           target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
+          rel="noreferrer noopener">
+          Repo</a>. Ok so i havent actually put anything here but i will eventually (probably).
       </p>
-      <br />
+      <br/>
       <h2>The date according to Go is:</h2>
       <p>{date ? date : 'Loading date...'}</p>
     </main>
