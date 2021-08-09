6cbe57e545e0615ee04bef13c7628680c1a0f671
fishyydev <19339@wc.school.nz>
2021-08-10 11:32:44 +1200

    hkjhdakad

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ function App() {
       <p>
         Ok so i havent actually put anything here but i will eventually (probably). I do have a <a href="https://shop.resailed.com/" rel="noopener">threadless shop</a> so check it out.
       </p>
-      <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/-1oRezdQEzM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <br/>
+      <iframe id="rick" width="560" height="315" src="https://www.youtube-nocookie.com/embed/-1oRezdQEzM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <br/>
       <h2>Here is the date in case u forgot:</h2>
       <p>{date ? date : 'Loading date...'}</p>
