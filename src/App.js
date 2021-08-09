f8d63792bba15acdbfcbb8e5d2620d90853adb01
fishyydev <19339@wc.school.nz>
2021-08-09 17:49:09 +1200

    update app.js

--- src/App.js
+++ src/App.js
@@ -15,26 +15,11 @@ function App() {
   return (
     <main>
       <h1>There is nothing here.</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
       <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/main/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener">
-          Repo</a>. Ok so i havent actually put anything here but i will eventually (probably).
+        Ok so i havent actually put anything here but i will eventually (probably).
       </p>
       <br/>
-      <h2>The date according to Go is:</h2>
+      <h2>Here is the date in case u forgot:</h2>
       <p>{date ? date : 'Loading date...'}</p>
     </main>
   );
